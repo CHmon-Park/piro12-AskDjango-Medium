5f6169463c882e610f4f39f7f66358632fed0767
ChanHyung-Park <pchrpg@naver.com>
2020-01-21 18:00:21 +0900

    3강

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required
+def profile(request):
+    request.user    # django.contrib.auth.models.AnonymousUser
+    return render(request, 'accounts/profile.html')
